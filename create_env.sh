1b5e83b8358fb893a83f3dcbaffd52538d056d0c
lagillenwater <lagillenwater@gmail.com>
2025-08-22 13:46:30 -0600

    update for running on alpine

--- create_env.sh
+++ create_env.sh
@@ -2,6 +2,9 @@
 
 # Create the dwpc_rnn_clean conda environment from environment.yml
 
+## for alpine
+module load anaconda
+
 conda env create -f environment.yml
 
 echo "Activate the environment with: conda activate dwpc_rnn"
