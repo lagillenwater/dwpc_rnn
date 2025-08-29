f10a162daf7555738b76340149f4a771146ec3e7
lagillenwater <lagillenwater@gmail.com>
2025-08-29 11:51:47 -0600

    permutattion script

---
+++ scripts/1_create_permuatations.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+#SBATCH --job-name=create_hetmat
+#SBATCH --account=amc-general
+#SBATCH --output=./logs/output_create_hetmat.log
+#SBATCH --error=./logs/error_create_hetmat.log
+#SBATCH --time=01:00:00
+#SBATCH --partition=amilan
+#SBATCH --qos=normal
+#SBATCH --ntasks-per-node=12
+#SBATCH --nodes=1 
+
+# Exit if any command fails
+set -e
+
+# Get the directory of this script and define base paths relative to it
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+BASE_DIR=$(realpath "$SLURM_SUBMIT_DIR/..")
+
+# Define relative data and output paths
+notebooks_path="${BASE_DIR}/notebooks"
+data_path="${BASE_DIR}/data"
+
+# Conda Environment:
+module load anaconda
+conda deactivate
+conda activate dwpc_rnn
+
+##########################################################################################################
+##########################################################################################################
+
+echo "****** permuting graph ******"
+
+
+input_notebook=${notebooks_path}/1_generate-permutations.ipynb
+output_notebook=${notebooks_path}/1_generate-permutations.ipynb
+
+papermill "$input_notebook" "$output_notebook" 
+
+
+# Deactivate the virtual environment w/ conda
+conda deactivate
